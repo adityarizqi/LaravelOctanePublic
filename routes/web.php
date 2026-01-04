8e2a5b8ec50bc9ff9a087a520404e982860d32b0
adityarizqi <adityarizqi1010@gmail.com>
2026-01-05 00:37:16 +0700

    (update) change homepage response to include PHP version

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return config('app.name') . ' - ' . 'Nova';
+    return config('app.name') . ' - ' . 'PHP ' . phpversion();
 });
